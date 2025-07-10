99b31b862414eeeebef0457a2bf3e39e1836f32d
dkhal10 <david@khalilis.net>
2025-07-10 13:40:17 -0400

    two sets of graphs

--- fastai/shadchan_model/male_model_analysis.py
+++ fastai/shadchan_model/male_model_analysis.py
@@ -1,32 +1,38 @@
 import pandas as pd
-import pandas as pd
 import shap
+import torch
 from fastai.tabular.all import *
 from sqlalchemy import create_engine, text
 import joblib
-
-
-learn = load_learner('male_model.pkl')
-
-engine = create_engine("postgresql+psycopg2://admin:admin@localhost:5432/postgres")
+engine = create_engine("postgresql+psycopg2://admin:admin@localhost:5432/SYAS")
 sql_query = text("SELECT * FROM total_males")
 with engine.connect() as conn:
     df = pd.read_sql(sql_query, conn)
 
-X = df[learn.dls.cont_names]
+# TRAITS MODEL
+learn_traits = load_learner('male_model_traits.pkl')
+X_traits = df[learn_traits.dls.cont_names]
 
-import torch
-from shap import DeepExplainer
+def predict_fn_traits(x):
+    return learn_traits.get_preds(dl=learn_traits.dls.test_dl(pd.DataFrame(x, columns=X_traits.columns)))[0].numpy()
 
-def predict_fn(x):
-    return learn.get_preds(dl=learn.dls.test_dl(pd.DataFrame(x, columns=X.columns)))[0].numpy()
+background_traits = shap.kmeans(X_traits, 20)
+explainer_traits = shap.KernelExplainer(predict_fn_traits, background_traits)
+X_small_traits = X_traits.reset_index(drop=True)
+shap_values_traits = explainer_traits.shap_values(X_small_traits)
 
-#background = X.sample(200, random_state=1)
-background = shap.kmeans(X, 20)
-explainer = shap.KernelExplainer(predict_fn, background)
+joblib.dump((shap_values_traits, X_small_traits), "shap_male_traits.pkl")
 
-X_small = X.reset_index(drop=True)
-shap_values = explainer.shap_values(X_small)
+# RELIGION MODEL ANALYSIS
+learn_religion = load_learner('male_model_religion.pkl')
+X_religion = df[learn_religion.dls.cont_names]  
 
-import joblib
-joblib.dump((shap_values, X_small), "shap_male.pkl")
+def predict_fn_religion(x):
+    return learn_religion.get_preds(dl=learn_religion.dls.test_dl(pd.DataFrame(x, columns=X_religion.columns)))[0].numpy()
+
+background_religion = shap.kmeans(X_religion, 20)
+explainer_religion = shap.KernelExplainer(predict_fn_religion, background_religion)
+X_small_religion = X_religion.reset_index(drop=True)
+shap_values_religion = explainer_religion.shap_values(X_small_religion)
+
+joblib.dump((shap_values_religion, X_small_religion), "shap_male_religion.pkl")
--- fastai/shadchan_model/test_female.py
+++ fastai/shadchan_model/test_female.py
@@ -3,12 +3,12 @@ import pandas as pd
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 learn = load_learner('female_model.pkl')
-engine = create_engine("postgresql+psycopg2://admin:admin@localhost:5432/postgres")
+engine = create_engine("postgresql+psycopg2://admin:admin@localhost:5432/SYAS")
 
 sql_query = text("SELECT * FROM member_values")
 with engine.connect() as conn:
     df = pd.read_sql(sql_query, conn) 
 
-row = df.iloc[0] 
+row = df.iloc[1] 
 result = learn.predict(row)
 print(result)
